e9385f27f765ed048d7aeba017ffc0ec08f14006
YKantsedal <159051956+YKantsedal@users.noreply.github.com>
2024-02-05 18:49:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,9 @@ streamlit.title('Modifying file with random text')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Will put some interaction form
+
+streamlit.multiselect("Pick something from here:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
