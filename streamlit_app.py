be0e1eb9d489c44415ec31aadeb873f48ea1025f
YKantsedal <159051956+YKantsedal@users.noreply.github.com>
2024-02-07 16:19:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title('Modifying file with random text')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Will put some interaction form
 
